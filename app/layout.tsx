42e8a36614c943cd8809a35333d374dcda372a8b
chris anatalio <chris@spheron.network>
2024-03-18 01:14:36 -0700

    Full end to end deployment flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  // without a title, warpcast won't validate your frame
-  title: "Deploy with Spheron on Farcaster",
-  description: "Deploy with Spheron on Farcaster",
-};
-
 export default function RootLayout({
   children,
 }: {
